541da9cfb9d3eb5759534b0ca9965ec1fb9f8559
James To <fol2hk@gmail.com>
2023-04-01 00:17:12 +0100

    finished first langchain lesson

--- langchain1.py
+++ langchain1.py
@@ -1,15 +1,12 @@
-pip install langchain
-
-pip install openai
-
 import os
 from langchain.llms import OpenAI
+import config
 
 # Put your OpenAI API key in the environment variable OPENAI_API_KEY
-os.environ["OPENAI_API_KEY"] = "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+os.environ["OPENAI_API_KEY"] = config.openaikey
 
 
 llm = OpenAI(temperature=0.9)
 
-text = "The quick brown fox jumps over the lazy dog."
+text = "What is the meaning of life?"
 print(llm(text))
